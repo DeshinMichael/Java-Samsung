51f239f39c65e36af9768603e3ab69fc07dca8db
DeshinMichael <deshinmisha26@gmail.com>
2022-10-29 18:12:01 +0800

    Vector

--- Test1/src/com/Training/Main.java
+++ Test1/src/com/Training/Main.java
@@ -2,26 +2,21 @@ package com.Training;
 
 public class Main {
     public static void main(String[] args) {
-        NDrob frac2 = new NDrob(4,5);
-        NDrob frac3 = new NDrob(3,5);
-        NDrob frac1 = frac2.del(frac3);
-        frac1.printDrob();
-//        frac1 = frac2.add(frac3);
-//        frac1.printDrob();
-        NDrob frac4 = frac2.sub(frac3);
-        frac4.printDrob();
-//        NDrob.count = 3;
-//        System.out.println(frac3.count);
-
-
-//        Square square1 = new Square(6);
-//        Rectangle rectangle = new Rectangle(3,6);
-//        System.out.println(square1);
-//        System.out.println(rectangle);
-//        System.out.println(square1.diagonalLength());
-//        System.out.println(rectangle.diagonalLength());
-//        System.out.println(square1.areaLength());
-//        System.out.println(rectangle.areaLength());
+        Vector2D vA = new Vector2D();
+        Vector2D vB = new Vector2D(5.0, 7.0);
+        Vector2D vC = new Vector2D(vB);
+        vA.add(vB);
+        vA.print();
+        vA.sub(vC);
+        vA.print();
+        System.out.println(vB.length());
+        vC.scale(1.5);
+        vC.print();
+        vC.normalized();
+        System.out.println(vC.length());
+        vC.scale(2);
+        System.out.println(vA.dotProduct(vB));
+        System.out.println(vA.count);
     }
 }
 
