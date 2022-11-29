45627aa30374e021a5e426495ee1c35ca63bf9d9
DeshinMichael <deshinmisha26@gmail.com>
2022-11-29 20:46:38 +0800

    DZ

---
+++ Test/app/src/main/java/com/example/test/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView logo;
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        logo = findViewById(R.id.logo);
+
+        intent = new Intent(SplashActivity.this, MainActivity.class);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo.setAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
